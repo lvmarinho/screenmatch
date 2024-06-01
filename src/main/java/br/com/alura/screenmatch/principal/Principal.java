b15cffe67892a239465ef2a36187e2dbd61363e1
leandro vitorino <arcanjo.lm@gmail.com>
2024-06-01 12:52:30 -0300

    buscando episódios, filtrando a partir de uma data e exibir data formatada

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.alura.screenmatch.model.Episodios;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -55,6 +57,23 @@ public class Principal {
                 .map(d -> new Episodios(t.numero(), d))
                 ).collect(Collectors.toList());
         episodios.forEach(System.out::println);
+
+        //buscando episódios, filtrando a partir de uma data e exibir data formatada// //
+
+        System.out.print("\nA partir de que ano você deseja ver os episodios?: ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null &&
+                        e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                   "Temporada: " + e.getTemporada() +
+                           "Episódio: " + e.getTitulo() +
+                           "Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
 
 
 
